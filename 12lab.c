74afe817ff890f2cbfa4fef7183d111d7e36b604
waple7 <91288621+waple7@users.noreply.github.com>
2021-12-16 21:48:16 +0300

    Update 12lab.c

--- 12lab.c
+++ 12lab.c
@@ -8,7 +8,7 @@ void make_directory(const char *name) {
 int main(int argc, char *argv[]) {
     FILE *fout;
     fout = fopen("waple.txt", "w");
-    fprintf(fout, "Subfolders of the current folder waple.txt: \n\n");
+    fprintf(fout, "Subfolders of the actual folder waple.txt: \n\n");
     if (argc > 1) {
         for (int i = 1; i < argc; i++) {
             make_directory(argv[i]);
