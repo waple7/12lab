a3124b773826106a41708e7c8d20b139c32c1d76
waple7 <91288621+waple7@users.noreply.github.com>
2021-12-17 11:16:22 +0300

    Update 12lab.c

--- 12lab.c
+++ 12lab.c
@@ -1,22 +1,20 @@
-#include <stdio.h>
-#include <direct.h>
+#include<stdio.h>
+#include<direct.h>
 
-void make_directory(const char *name) {
-    _mkdir(name);
-}
-
-int main(int argc, char *argv[]) {
-    FILE *fout;
-    char name[100];
-    scanf("%[^\n]s",&name);
-    fout = fopen(name, "w");
-    fprintf(fout, "Subfolders of the actual folder waple.txt: \n\n");
-    if (argc > 1) {
-        for (int i = 1; i < argc; i++) {
-            make_directory(argv[i]);
-            fprintf(fout, "%s\n", argv[i]);
+int main(int argc,char* argv[]){
+    FILE* fout;
+    fopen_s(&fout,"game.txt","w");
+    int p=1;
+    while(p>0){
+        char s[80];
+        gets(s);
+        if(s[0]=='*')
+            break;
+        else {
+            fprintf(fout, "%s\n", s);
+            fflush(fout);
+            _mkdir(s);
         }
     }
     fclose(fout);
-    return 0;
 }
